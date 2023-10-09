218db63af0dacc8c92d06602794c93cb0e1a7df0
kaiser <henridev3@gmail.com>
2023-10-09 16:19:24 -0300

    explicação das variaveis

---
+++ js/aula01.js
@@ -0,0 +1,24 @@
+/* multiplas linhas */
+// comentario de uma só linha
+
+// string= conjunto de catacteres, pode ser seu nome:
+// var Nome = 'henrique'
+
+// tipos de informações
+/* 
+Float= 9.0
+Int= 9
+boolean= true/false
+*/
+
+//string, numerico, boolean
+
+
+var nome = 'henrique'
+//escopo global - redeclarar a variavel e setar um novo valor
+let sobrenome = 'ribeiro'
+// não pode redeclarar - posso alterar o valor 
+const CPF = '48832152886'
+//valor unico e não pode ser redeclarado
+
+//extensões code runner, ESlint, JavaScript ES6
