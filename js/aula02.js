13c20e83f2f6377bc35c338834ac26a08d65730d
kaiser <henridev3@gmail.com>
2023-10-10 14:32:40 -0300

    aula02

---
+++ js/aula02.js
@@ -0,0 +1,16 @@
+//""
+//'' = concatenação = junta de strings
+//`` = interpolação = mais util
+
+let aluno = 'Henrique';
+let nota1 = 5.1;
+let nota2 = 9;
+
+console.log(`A média do ${aluno} é: ${(nota1 + nota2) / 2}`);
+
+let i = "istituto ";
+let o = "oportunidade ";
+let s = "social";
+
+console.log(i + 'da ' + o + s);
+console.log(`${i}da ${o}${s}`);
