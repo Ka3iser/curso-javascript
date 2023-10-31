20ff085d67632f41f0d63300da22f21c8207cfbb
kaiser <henridev3@gmail.com>
2023-10-31 13:45:54 -0300

    while

---
+++ js/aula03.js
@@ -0,0 +1,22 @@
+let disciplina = "quarta"
+
+switch (disciplina) {
+    case "Segunda":
+        console.log(`estudar de português`);
+        break;
+    case "terça":
+        console.log(`estudar de quimica`);
+        break;
+    case "quarta":
+        console.log(`estudar de matematica`);
+        break;
+    case "quinta":
+        console.log(`estudar de ciencia`);
+        break;
+    case "sexta":
+        console.log(`estudar de biologia`);
+        break;
+    default:
+        console.log(`não estudar`);
+        break;
+}
---
+++ js/while.js
@@ -0,0 +1,23 @@
+let chocolate = 10
+
+while (chocolate > 0) {
+    console.log(`comer chocolate, quanto ainda tenho? ${chocolate}`);
+    chocolate--
+}
+
+/* let contador = 0
+
+while (contador <= 100) {
+    console.log(`contagem está em: ${contador}`);
+    contador += 5
+} */
+
+/* do {
+    console.log(`contagem está em: ${contador}`);
+    contador += 5
+} while (contador <= 100); */
+
+for(let contador = 0; contador <= 5; contador++){
+    console.log(`contagem está em: ${contador}`);
+}
+
